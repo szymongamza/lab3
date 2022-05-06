6183e190c841b9703dceeda767fc9abf02d5e13b
Szymon Gamża <gamza.szymon@gmail.com>
2022-05-06 21:16:15 +0200

    Changed view responsiveness

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,6 +2,8 @@ import Students.StudentCondition;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.event.*;
 
 public class MainFrame extends JFrame implements ActionListener {
@@ -11,13 +13,17 @@ public class MainFrame extends JFrame implements ActionListener {
     private JTable studentTable, groupTable;
     private JScrollPane studentPane, groupPane;
     private JButton jButtonNewStudent, jButtonDeleteStudent, jButtonEditStudent, jButtonNewGroup, jButtonDeleteGroup, jButtonEditGroup, jButtonSort;
+    private TableRowSorter tableRowSorter;
+    private JTextField jtextField;
 
     public MainFrame() {
         jFrame = new JFrame("StudentTable");
-        jFrame.setPreferredSize((new Dimension(1000, 500)));
+        //jFrame.setPreferredSize((new Dimension(1100, 600)));
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
+
+
 
         groupTableModel = new GroupTableModel();
         groupTable = new JTable(groupTableModel);
@@ -30,16 +36,18 @@ public class MainFrame extends JFrame implements ActionListener {
             }
         });
 
-        groupTable.setPreferredScrollableViewportSize(new Dimension(450, 400));
+        //groupTable.setPreferredScrollableViewportSize(new Dimension(450, 300));
         groupTable.setAutoCreateRowSorter(true);
         groupPane = new JScrollPane(groupTable);
 
         studentTableModel = new StudentTableModel();
         studentTable = new JTable(studentTableModel);
 
-        studentTable.setPreferredScrollableViewportSize(new Dimension(450, 400));
-        studentTable.setAutoCreateRowSorter(true);
+        //studentTable.setPreferredScrollableViewportSize(new Dimension(450, 300));
         studentPane = new JScrollPane(studentTable);
+
+        tableRowSorter = new TableRowSorter<StudentTableModel>(studentTableModel);
+        studentTable.setRowSorter(tableRowSorter);
 
         jButtonNewGroup = new JButton("New Group");
         jButtonNewGroup.addActionListener(this);
@@ -62,9 +70,24 @@ public class MainFrame extends JFrame implements ActionListener {
         jButtonDeleteStudent = new JButton("Delete Student");
         jButtonDeleteStudent.addActionListener(this);
 
+        jtextField = new JTextField();
+        //jtextField.setPreferredSize(new Dimension(800,25));
+        jtextField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == 10){
+                    String query = jtextField.getText();
+                    filter(query);
+                }
+            }
+        });
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new Insets(5,5,5,5);
         c.gridwidth = 3;
         c.gridx = 0;
         c.gridy = 0;
+        c.weightx = 0.5;
+        c.weighty = 0.9;
         jFrame.getContentPane().add(groupPane, c);
 
         c.gridwidth = 3;
@@ -72,40 +95,58 @@ public class MainFrame extends JFrame implements ActionListener {
         c.gridy = 0;
         jFrame.getContentPane().add(studentPane, c);
 
-        c.gridwidth = 1;
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1;
+        c.weighty = 0.01;
+        c.gridwidth = 7;
         c.gridx = 0;
         c.gridy = 1;
+        jFrame.getContentPane().add(jtextField,c);
+
+
+        c.anchor = GridBagConstraints.PAGE_END;
+        c.weighty = 0.01;
+        c.weightx = 0.1;
+        c.gridwidth = 1;
+        c.gridx = 0;
+        c.gridy = 2;
         jFrame.getContentPane().add(jButtonNewGroup, c);
 
         c.gridx = 1;
-        c.gridy = 1;
+        c.gridy = 2;
         jFrame.getContentPane().add(jButtonEditGroup, c);
 
         c.gridx = 2;
-        c.gridy = 1;
+        c.gridy = 2;
         jFrame.getContentPane().add(jButtonDeleteGroup, c);
 
         c.gridx = 3;
-        c.gridy = 1;
+        c.gridy = 2;
         jFrame.getContentPane().add(jButtonSort, c);
 
         c.gridx = 4;
-        c.gridy = 1;
+        c.gridy = 2;
         jFrame.getContentPane().add(jButtonNewStudent, c);
 
         c.gridx = 5;
-        c.gridy = 1;
+        c.gridy = 2;
         jFrame.getContentPane().add(jButtonEditStudent, c);
 
         c.gridx = 6;
-        c.gridy = 1;
+        c.gridy = 2;
         jFrame.getContentPane().add(jButtonDeleteStudent, c);
 
         jFrame.setVisible(true);
         jFrame.pack();
 
 
+
     }
+    private void filter(String query){
+        tableRowSorter.setRowFilter(RowFilter.regexFilter(query));
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
